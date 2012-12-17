5bfa614cf2e779587ade54b6e5324e34360de151
Kent <none@none>
2012-12-17 20:26:29 +0100

    change the way to get current dir in start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-a=`dirname $0`
-if [ $a = '.' ];then
-   a=`pwd`
+
+# find current directory
+script_path="${BASH_SOURCE[0]}"
+if ([ -L "${script_path}" ]) then
+	while ([ -L "${script_path}" ]) do script_path=$(readlink "${script_path}"); done
 fi
-current=$a
-echo "current path:"$current
-cd $current
+pushd . > /dev/null
+cd $(dirname ${script_path}) > /dev/null
+script_path=$(pwd)
+echo "current path:"$script_path
 python passwdManager.py
